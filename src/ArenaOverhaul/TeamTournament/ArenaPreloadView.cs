aa23bc341364b5d7ca9bb17fb3421950607f82db
Alexey Chernyshov <artifixer@gmail.com>
2023-01-22 18:59:41 +0300

    Miscellaneous fixes

---
+++ src/ArenaOverhaul/TeamTournament/ArenaPreloadView.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade.View.MissionViews;
+using TaleWorlds.MountAndBlade.View;
+using TaleWorlds.MountAndBlade;
+
+namespace ArenaOverhaul.TeamTournament
+{
+    internal class ArenaPreloadView : MissionView
+    {
+        private readonly PreloadHelper _helperInstance = new PreloadHelper();
+        private bool _preloadDone;
+
+        public override void OnPreMissionTick(float dt)
+        {
+            if (_preloadDone)
+                return;
+            List<BasicCharacterObject> characters = new();
+            TeamTournamentBehavior missionBehavior = Mission.Current.GetMissionBehavior<TeamTournamentBehavior>();
+            if (missionBehavior != null)
+            {
+                foreach (CharacterObject possibleParticipant in missionBehavior.GetAllPossibleParticipants())
+                    characters.Add(possibleParticipant);
+            }
+            _helperInstance.PreloadCharacters(characters);
+            _preloadDone = true;
+        }
+
+        public override void OnSceneRenderingStarted() => _helperInstance.WaitForMeshesToBeLoaded();
+
+        public override void OnMissionStateDeactivated()
+        {
+            base.OnMissionStateDeactivated();
+            _helperInstance.Clear();
+        }
+    }
+}
