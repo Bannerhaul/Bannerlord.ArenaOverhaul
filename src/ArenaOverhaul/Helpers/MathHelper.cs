9743103518503a8563700c4657a25278cc1245b3
Alexey Chernyshov <artifixer@gmail.com>
2023-02-01 11:57:45 +0300

    Tiny refactoring

--- src/ArenaOverhaul/Helpers/MathHelper.cs
+++ src/ArenaOverhaul/Helpers/MathHelper.cs
@@ -6,8 +6,8 @@ namespace ArenaOverhaul.Helpers
     {
         public static int GetSoftCappedValue(float value, int softCap = 10000)
         {
-            int baseLog = (int) Math.Max(Math.Log10(softCap) - 1, 0);
-            return (int) (value <= softCap ? value : (softCap * (Math.Log10(value) - baseLog)));
+            int softCapLog = (int) Math.Max(Math.Log10(softCap) - 1, 0);
+            return (int) (value <= softCap ? value : (softCap * (Math.Log10(value) - softCapLog)));
         }
     }
 }
